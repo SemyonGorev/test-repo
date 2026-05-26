7af09b46c9b572d8dbce8a0ba925efdff130afcc
SemyonGorev <doc.sam1999@gmail.com>
2026-05-26 19:01:34 +0300

    Add files via upload

---
+++ lesson3/03_leseson/smartphone.py
@@ -0,0 +1,12 @@
+class Smartphone:
+
+    def __init__(self, brand, model, number):
+        self.brand = brand
+        self.model = model
+        self.number = number
+    def getBrand(self):
+        print(self.brand)
+    def getModel(self):
+        print(self.model)
+    def getNumber(self):
+        print(self.number)
