1da5f6c0ab475186541823c64c4f971786073999
SemyonGorev <doc.sam1999@gmail.com>
2026-07-02 14:09:05 +0300

    Add files via upload

--- lesson7/shoppage.py
+++ lesson7/shoppage.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class Shop_Log_In:
     def __init__(self, driver):
         self.driver = driver
@@ -14,34 +15,43 @@ class Shop_Log_In:
 
     def password(self):
         self.driver.find_element(By.ID, 'password').send_keys('secret_sauce')
-    
+
     def login_btn(self):
         self.driver.find_element(By.ID, 'login-button').click()
+
 
 class Shop_Main_Page:
     def __init__(self, driver):
         self.driver = driver
-    
+
     def add_Sauce_Labs_Backpack(self):
-        self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
+        self.driver.find_element(
+            By.ID, 'add-to-cart-sauce-labs-backpack').click()
 
     def add_Sauce_Labs_Bikelight(self):
-        self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-bike-light').click()
+        self.driver.find_element(
+            By.ID, 'add-to-cart-sauce-labs-bike-light').click()
 
     def add_Sauce_Labs_Tshirt(self):
-        self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-bolt-t-shirt').click()
+        self.driver.find_element(
+            By.ID, 'add-to-cart-sauce-labs-bolt-t-shirt').click()
 
     def add_Sauce_Labs_Fleece_Jacket(self):
-        self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-fleece-jacket').click()
+        self.driver.find_element(
+            By.ID, 'add-to-cart-sauce-labs-fleece-jacket').click()
 
     def add_Sauce_Labs_Onesie(self):
-        self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-onesie').click()
+        self.driver.find_element(
+            By.ID, 'add-to-cart-sauce-labs-onesie').click()
 
     def add_Sauce_Labs_Red_Tshirt(self):
-        self.driver.find_element(By.ID, 'add-to-cart-test.allthethings()-t-shirt-(red)').click()
+        self.driver.find_element(
+            By.ID, 'add-to-cart-test.allthethings()-t-shirt-(red)').click()
 
     def go_to_cart(self):
-        self.driver.find_element(By.CSS_SELECTOR, '.shopping_cart_link').click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, '.shopping_cart_link').click()
+
 
 class Cart:
     def __init__(self, driver):
@@ -50,8 +60,10 @@ class Cart:
     def checkout(self):
         self.driver.find_element(By.ID, 'checkout').click()
 
+
 class Checkout_Page:
     TOTAL_VALUE = (By.CLASS_NAME, "summary_total_label")
+
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 45)
@@ -69,7 +81,7 @@ class Checkout_Page:
         self.driver.find_element(By.ID, 'continue').click()
 
     def get_result(self):
-        self.wait.until(EC.text_to_be_present_in_element(self.TOTAL_VALUE, "Total: $58.29"))
+        self.wait.until(EC.text_to_be_present_in_element
+                        (self.TOTAL_VALUE, "Total: $58.29"))
         result_element = self.driver.find_element(*self.TOTAL_VALUE)
         return result_element.text
-
